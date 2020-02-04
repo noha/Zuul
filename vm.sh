e2044434049e9ef944afe16c1cb4013d788cde38
Norbert Hartl <norbert@hartl.name>
2020-02-04 11:58:39 +0100

    corrected path

--- vm.sh
+++ vm.sh
@@ -5,7 +5,7 @@ INSTALL_DIR=`dirname $0`
 
 if [ "$LABEL" == "" ] ; 
 then 
-    $INSTALL_DIR/linux/pharo $*
+    $INSTALL_DIR/linux-vm/pharo $*
 else
     $INSTALL_DIR/macos-vm/Pharo.app/Contents/MacOS/Pharo $*
 fi
